b46afcd9c52cc4378941e2e832af7e3ea34e6601
chan <djc0807285@gmail.com>
2025-10-18 01:26:18 +0900

    feat: 커스텀 구분자 기능 추가

--- src/main/java/calculator/StrCalculator.java
+++ src/main/java/calculator/StrCalculator.java
@@ -1,5 +1,4 @@
 package calculator;
-import java.lang.String;
 
 public class StrCalculator {
     //빈 문자열 처리
@@ -8,16 +7,33 @@ public class StrCalculator {
     }
 
     public int add(String s) {
+
         //빈 문자열 확인
         if (isBlank(s)) {
             return 0;
         }
-        // ,: 구분자
-        String[] WODelimiter = s.split("[,:]");
+
+        //변수 초기화
+        String delimiter = "[,:]";
+        String text = s;
+
+
+        //커스텀 구분자 처리
+        if(s.startsWith("//")){
+            int Index = s.indexOf("n")+1;
+            delimiter = s.substring(2, 3);
+            text = s.substring(Index);
+
+        }
+
+        String[] WODelimiter = text.split(delimiter);
+
 
         int sum = 0;
         for (String string : WODelimiter) {
-            sum += Integer.parseInt(string);
+            if (!string.isEmpty()) {
+                sum += Integer.parseInt(string);
+            }
         }
 
         return sum;
